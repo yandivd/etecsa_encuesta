7d7137e149404fb004bd66e24ec75a4a155e6b3c
yandivd <yandivd@gmail.com>
2021-11-26 11:41:16 -0500

    espacio entre los import y models

--- encuesta/models.py
+++ encuesta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+
 class Area_Organizativa(models.Model):
     nombre=models.CharField(max_length=150, unique=True)
 
